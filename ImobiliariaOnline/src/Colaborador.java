79c4bca52b5a4fb0eda75d3e112a2546fa1ecd40
fernandamotad <131628531+fernandamotaDCOMP@users.noreply.github.com>
2023-10-17 12:40:49 -0300

    alterações

--- ImobiliariaOnline/src/Colaborador.java
+++ ImobiliariaOnline/src/Colaborador.java
@@ -1,46 +1,42 @@
-import java.util.ArrayList;
 import java.util.List;
-
-public class Colaborador extends Login{
+import java.util.ArrayList;
+public class Colaborador extends Login {
     private List<Colaborador> colaboradoresCadastrados;
     private List<Cliente> clientesCadastrados;
-    public Colaborador(String acessoAdm, String chaveDeAcessoColaborador){
+
+    public Colaborador(String acessoAdm, String chaveDeAcessoColaborador) {
         super(acessoAdm, chaveDeAcessoColaborador);
     }
 
-    // Cadastro de alguns colaboradores
-    Colaborador colaboradoresCadastrados = new ArrayList<>();
+    public Colaborador() {
+        colaboradoresCadastrados = new ArrayList<>();
+
+        // Adicionando colaboradores à lista
         colaboradoresCadastrados.add(new Colaborador("Colaborador1", "2023001"));
         colaboradoresCadastrados.add(new Colaborador("Colaborador2", "2023002"));
+    }
 
-
-    public void adicionarCliente(){
+    public void adicionarCliente() {
         clientesCadastrados = new ArrayList<>();
 
-        //Simulando alguns clientes cadastrados
+        // Simulando alguns clientes cadastrados
         clientesCadastrados.add(new Cliente("0001/2023", "senha123"));
         clientesCadastrados.add(new Cliente("0002/2023", "abc123"));
-
     }
-    public void selecionarProcesso(){
 
+    public void selecionarProcesso() {
+        // Implemente o código para selecionar um processo
     }
-    public void selecionarEtapaAtual(){
 
+    public void selecionarEtapaAtual() {
+        // Implemente o código para selecionar a etapa atual
     }
-    public void selecionarEtapa(){
 
+    public void selecionarEtapa() {
+        // Implemente o código para selecionar uma etapa
     }
-    public void adicionarEtapa(){
 
-    }
-    public void excluirEtapa(){
-
-    }
-    public void criarProcesso(){
-
-    }
-    public void excluirProcesso(){
-
+    public void adicionarEtapa() {
+        // Implemente o código
     }
 }
--- ImobiliariaOnline/src/TelaDeLogin.java
+++ ImobiliariaOnline/src/TelaDeLogin.java
@@ -1,74 +1,110 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.List; // Importe a classe List correta
+import java.awt.event.*;
 
 public class TelaDeLogin extends JFrame {
-    private JTextField campoUsuario;
-    private JPasswordField campoSenha;
-    private List<Cliente> clientesCadastrados;
+    private JRadioButton clienteRadio;
+    private JRadioButton colaboradorRadio;
 
     public TelaDeLogin() {
-        super("Tela de Login");
+        setTitle("Login");
+        setSize(1200, 700);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 150);
         setLocationRelativeTo(null);
 
+        // Barra laranja
+        JPanel headerPanel = new JPanel();
+        headerPanel.setBackground(Color.decode("#FF8210"));
+        headerPanel.setPreferredSize(new Dimension(getWidth(), 40));
+
+        // Painel para o conteúdo central
+        JPanel contentPanel = new JPanel(new GridBagLayout());
+        contentPanel.setBackground(Color.WHITE); // Fundo branco
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
 
 
-        JPanel painel = new JPanel();
-        painel.setLayout(new GridLayout(3, 2));
+        JLabel labelTitulo = new JLabel("Imobiliária Online");
+        labelTitulo.setFont(new Font("Arial", Font.BOLD, 40));
 
-        JLabel labelUsuario = new JLabel("Usuário:");
-        JLabel labelSenha = new JLabel("Senha:");
-        campoUsuario = new JTextField();
-        campoSenha = new JPasswordField();
+        // Painel para botões de rádio (cliente e colaborador)
+        JPanel radioPanel = new JPanel();
+        radioPanel.setLayout(new BoxLayout(radioPanel, BoxLayout.X_AXIS));
+        radioPanel.setBackground(Color.WHITE); // Fundo branco
 
-        JButton botaoLogin = new JButton("Login");
-        botaoLogin.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                realizarLogin();
-            }
-        });
+        ButtonGroup radioGroup = new ButtonGroup();
 
+        clienteRadio = new JRadioButton("Cliente");
+        clienteRadio.setBackground(Color.WHITE); // Fundo branco
+        radioGroup.add(clienteRadio);
 
-        painel.add(labelUsuario);
-        painel.add(campoUsuario);
-        painel.add(labelSenha);
-        painel.add(campoSenha);
-        painel.add(new JLabel());
-        painel.add(botaoLogin);
+        colaboradorRadio = new JRadioButton("Colaborador");
+        colaboradorRadio.setBackground(Color.WHITE); // Fundo branco
+        radioGroup.add(colaboradorRadio);
 
-        add(painel);
-        setVisible(true);
+        JPanel camposPanel = new JPanel(new GridBagLayout());
+        camposPanel.setBackground(Color.WHITE); // Fundo branco
+
+        gbc.anchor = GridBagConstraints.WEST; // Alinhar à esquerda
+
+        JLabel labelUsuario = new JLabel("USUÁRIO");
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        camposPanel.add(labelUsuario, gbc);
+
+        JTextField campoUsuario = new JTextField();
+        campoUsuario.setPreferredSize(new Dimension(300, 40)); // Tamanho fixo
+        gbc.gridy = 1;
+        camposPanel.add(campoUsuario, gbc);
+        labelUsuario.setFont(new Font("Arial", Font.BOLD, 14));
+
+        JLabel labelSenha = new JLabel("SENHA");
+        gbc.gridy = 2;
+        camposPanel.add(labelSenha, gbc);
+        labelSenha.setFont(new Font("Arial", Font.BOLD, 14));
+
+        JPasswordField campoSenha = new JPasswordField();
+        campoSenha.setPreferredSize(new Dimension(300, 40)); // Tamanho fixo
+        gbc.gridy = 3;
+        camposPanel.add(campoSenha, gbc);
+
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        contentPanel.add(labelTitulo, gbc);
+
+        gbc.gridy = 1;
+        contentPanel.add(radioPanel, gbc);
+
+        gbc.gridy = 2;
+        contentPanel.add(Box.createRigidArea(new Dimension(0, 20)), gbc);
+
+        gbc.gridy = 3;
+        contentPanel.add(camposPanel, gbc);
+
+        radioPanel.add(Box.createRigidArea(new Dimension(10, 0))); // Espaço
+        radioPanel.add(clienteRadio);
+        radioPanel.add(Box.createRigidArea(new Dimension(10, 0))); // Espaço
+        radioPanel.add(colaboradorRadio);
+
+        JButton botaoLogin = new JButton("Fazer Login");
+        botaoLogin.setPreferredSize(new Dimension(132, 40));
+        botaoLogin.setBackground(Color.decode("#FF8210")); // Fundo laranja
+        botaoLogin.setForeground(Color.WHITE); // Fonte branca
+        botaoLogin.setFont(new Font("Arial", Font.BOLD, 14)); // Texto em negrito
+        gbc.gridy = 4;
+        contentPanel.add(botaoLogin, gbc);
+
+        add(headerPanel, BorderLayout.NORTH);
+        add(contentPanel, BorderLayout.CENTER);
     }
 
-    private void realizarLogin() {
-        String usuario = campoUsuario.getText();
-        String senha = new String(campoSenha.getPassword());
-
-        // Verificar se as credenciais correspondem a um cliente cadastrado
-        boolean autenticado = false;
-        for (Cliente cliente : clientesCadastrados) {
-            if (cliente.getSenha().equals(senha) && cliente.getUsuario().equals(usuario)) {
-                autenticado = true;
-                JOptionPane.showMessageDialog(this, "Login bem-sucedido! Bem-vindo, cliente!");
-                break;
-            }
-        }
-        // Verifica se as credenciais pertencem a algum colaborador cadastrado
-        for (Colaborador colaborador : colaboradoresCadastrados) {
-            if (colaborador.getSenha().equals(senha) && colaborador.getUsuario().equals(usuario)) {
-                autenticado = true;
-                JOptionPane.showMessageDialog(this, "Login bem-sucedido! Bem-vindo, colaborador!");
-                break;
-            }
-        }
-
-        if (!autenticado) {
-            JOptionPane.showMessageDialog(this, "Falha no login. Verifique suas credenciais.");
-        }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            TelaLogin telaLogin = new TelaLogin();
+            telaLogin.setVisible(true);
+        });
     }
 }
