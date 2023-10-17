7dfa961341997d634ebcc7e5e18adf3e41eaceeb
fernandamotad <131628531+fernandamotaDCOMP@users.noreply.github.com>
2023-10-17 12:02:57 -0300

    Novos métodos em Colaborador

--- ImobiliariaOnline/src/Colaborador.java
+++ ImobiliariaOnline/src/Colaborador.java
@@ -1,6 +1,26 @@
 public class Colaborador extends Login{
-    public Colaborador(String numeroProcesso, String chaveDeAcessoColaborador){
-        super(numeroProcesso, chaveDeAcessoColaborador);
+    public Colaborador(String acessoGeral, String chaveDeAcessoColaborador){
+        super(acessoGeral, chaveDeAcessoColaborador);
     }
+    public void selecionarProcesso(){
 
+    }
+    public void selecionarEtapaAtual(){
+
+    }
+    public void selecionarEtapa(){
+
+    }
+    public void adicionarEtapa(){
+
+    }
+    public void excluirEtapa(){
+
+    }
+    public void criarProcesso(){
+
+    }
+    public void excluirProcesso(){
+
+    }
 }
